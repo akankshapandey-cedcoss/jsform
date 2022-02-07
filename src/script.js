b589c5bd42293182e45d9fae6e393846160bae59
akankshapandey-cedcoss <akankshapandey@cedcoss.com>
2022-02-07 11:21:00 +0530

    first commit

---
+++ src/script.js
@@ -0,0 +1,9 @@
+function display()
+{
+var x=document.details.pid.value;
+var y=document.details.pname.value;
+var z=document.details.pprice.value;
+alert("product id:"+x+"     "+"product name:"+y+"     "+"product price:"+z);
+
+
+}
